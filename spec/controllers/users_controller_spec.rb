4a04a6bd8cc4dce98df71381f7e0b1f0fa56fecc
gomzaemon111 <goki14914@icloud.com>
2018-07-09 20:46:51 +0900

    fix

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,7 +4,7 @@ describe UsersController, type: :controller do
   describe 'GET #index' do
     it "render the :index template" do
       get :index
-      expect(response).to render_template :index
+      expect(response).to have_http_status(200)
     end
   end
 end
