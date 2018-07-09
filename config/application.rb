9ae1b1c9396b05be5eec485a8dd5156a556378a0
gomzaemon111 <goki14914@icloud.com>
2018-07-09 20:26:09 +0900

    fix configuration

--- config/application.rb
+++ config/application.rb
@@ -14,17 +14,17 @@ module Xtreme
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-+    config.generators do |g|
-+      g.test_framework :rspec,
-+        fixtures: true,         # モデル作成時にフィクスチャの作成を有効化(後述のfactory_girlが適用される)
-+        view_specs: false,      # 以下、必要に応じて任意にトグルする
-+        helper_specs: false,
-+        routing_specs: false,
-+        controller_specs: false,
-+        request_specs: false
-+
-+      # fixtureの代わりにfactory_girlを使うよう設定
-+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
-+    end
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,         # モデル作成時にフィクスチャの作成を有効化(後述のfactory_girlが適用される)
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+
+      # fixtureの代わりにfactory_girlを使うよう設定
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
