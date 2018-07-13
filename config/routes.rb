ad397c06f7260f61fed9dacf70055b364e175941
gonzaemon111 <goki14914@icloud.com>
2018-07-14 02:25:17 +0900

    add api and some nest in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  resources :task
+  resources :tasks
   resources :users do
     resources :groups
     resources :members
+  end
+
+  name_space :api do
+    resources :users do
+      resources :groups do
+        resources :comments, only [:create]
+        resources :memos, only [:create]
+      end
+    end
   end
   # mount API::Root => '/api'
 end
