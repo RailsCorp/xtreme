fa638caa7c13a46fe681698e36070b28745eb373
神谷健太 <kentakmy@gmail.com>
2018-07-17 19:46:27 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,18 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index' => 'home#index'
   resources :users do
-    resources :groups, only: %i[:index]
-    resources :members, only: %i[:index]
-    resources :coporations, only: %i[:show]
+    resources :groups, only: [:index]
+    resources :members, only: [:index]
+    resources :coporations, only: [:show]
     resources :tasks
   end
 
   namespace :api do
     resources :users do
       resources :groups do
-        resources :comments, only: %i[:create, :destory]
-        resources :memos, only: %i[:create, :destory]
-        resources :coporations, only: %i[:create, :destory, :update]
+        resources :comments, only: [:create, :destory]
+        resources :memos, only: [:create, :destory]
+        resources :coporations, only: [:create, :destory, :update]
       end
     end
   end
