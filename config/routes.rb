914aba36be4b4d205506bfdebfe612e76b46e2c3
gonzaemon111 <goki14914@icloud.com>
2018-07-24 00:21:13 +0900

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,19 @@
 Rails.application.routes.draw do
-  get 'coporations/show'
-
   devise_for :users
   get 'home/index' => 'home#index'
+  resources :tasks, only: :index
   resources :users do
-    resources :groups, only: [:index]
-    resources :members, only: [:index]
-    resources :coporations, only: [:show]
-    resources :tasks
   end
 
   namespace :api do
     resources :users do
-      resources :groups do
-        resources :comments, only: [:create, :destory]
-        resources :memos, only: [:create, :destory]
-        resources :coporations, only: [:create, :destory, :update]
-      end
+      resources :groups
+      resources :coporations
+    end
+
+    resources :tasks, only: [:create, :update, :destroy] do
+      resources :comments, only: [:create, :destory]
+      resources :memos, only: [:create, :destory]
     end
   end
   # mount API::Root => '/api'
