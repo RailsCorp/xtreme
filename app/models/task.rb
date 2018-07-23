d58806d3371c753c8b85900e971e2ebf2b35591f
gonzaemon111 <goki14914@icloud.com>
2018-07-23 17:46:05 +0900

    fix model about task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,5 @@
 class Task < ApplicationRecord
-  belongs_to :user
-  belongs_to :group
+  enum task_type: [:user, :group]
   has_many :comments, dependent: :destroy
   has_many :memos, dependent: :destroy
   validates :task, presense: true
