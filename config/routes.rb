275b680c24776aa5ca3d143eb9945ce8668dc37f
gonzaemon111 <goki14914@icloud.com>
2018-08-03 03:17:35 +0900

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,13 @@ Rails.application.routes.draw do
   resources :groups, only: %i[show]
 
   namespace :api do
-    resources :users do
-      resources :groups
-      resources :coporations
-    end
-
+    # resources :users
     resources :groups, only: %i[show] do
-      resources :group_tasks, %i[index create update destroy]
+      resources :group_tasks, only: %i[index create update destroy]
     end
-
     resources :tasks, only: %i[create update destroy] do
       resources :comments, only: %i[create destory]
       resources :memos, only: %i[create destory]
     end
   end
-  # mount API::Root => '/api'
 end
