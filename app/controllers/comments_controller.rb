8f8a00569f0756159e74f6950676c80bfd790799
gonzaemon111 <goki14914@icloud.com>
2018-08-08 03:39:20 +0900

    add app/controllers/comments and memos controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,42 @@
+class CommentsController < ApplicationController
+  before_action :set_user
+  before_action :set_task
+
+  def index
+    @comments =
+      Comment.
+        with_task.
+        with_user.
+        order("id DESC")
+
+    @comments = CommentDecorator.decorate_collection(@comments)
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      @comment = CommentDecorator.decorate(@comment)
+    else
+      raise "error"
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_task
+    @task = Task.find(params[:task_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(
+      :text
+    ).merge(
+      user_id: @user.id,
+      task_id: @task.id
+    )
+  end
+end
