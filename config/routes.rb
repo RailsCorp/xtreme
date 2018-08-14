dd77e9f33bae691e1076c685607e04e6fbde744a
gonzaemon111 <goki14914@icloud.com>
2018-08-14 21:41:45 +0900

    add /admin in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "groups#show"
   get "home/index" => "home#index"
+  resources :users, only: %i[show update destroy]
   resources :groups, only: %i[create update destroy show] do
     resources :tasks, only: %i[index create update destroy show]
     resources :members, only: %i[index create update destroy]
@@ -9,5 +10,10 @@ Rails.application.routes.draw do
   resources :tasks, only: %i[index create update destroy show] do
     resources :comments, only: %i[index create]
     resources :memos, only: %i[index create]
+  end
+  # 管理画面用
+  namespace :admin do
+    devise_for :admin_users
+    resources :admin_users, only: %i[show update destroy]
   end
 end
