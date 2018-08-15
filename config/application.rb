3a16a50541692953d7131f79d338082829dc15b3
gonzaemon111 <goki14914@icloud.com>
2018-08-15 19:01:06 +0900

    delete cpnfiguration for grape of gem

--- config/application.rb
+++ config/application.rb
@@ -10,16 +10,10 @@ module Xtreme
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.paths.add File.join("app", "apis"), glob: File.join("**", "*.rb")
     config.autoload_paths += Dir[Rails.root.join("app", "apis", "*")]
     config.time_zone = "Tokyo"
     config.i18n.default_locale = :ja
     config.logger = ActiveSupport::Logger.new("log/test.log")
-
-    # Grape+JBuilderを使うための設定
-    config.middleware.use(Rack::Config) do |env|
-      env["api.tilt.root"] = Rails.root.join "app", "views", "api"
-    end
 
     config.generators do |g|
       g.assets false
