43ee67a6659e4b5ef2397465a76ffc8a5e577733
gonzaemon111 <goki14914@icloud.com>
2018-08-16 05:38:43 +0900

    fix for image at Faker

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :group do
     name "test"
     introduce "hogehoge"
-    image "test.png"
+    image Rack::Test::UploadedFile.new(File.join(Rails.root, "public/images/img_0813.png"))
     created_at { Faker::Time.between(5.days.ago, 3.days.ago, :all) }
     updated_at { Faker::Time.between(2.days.ago, 1.days.ago, :all) }
   end
